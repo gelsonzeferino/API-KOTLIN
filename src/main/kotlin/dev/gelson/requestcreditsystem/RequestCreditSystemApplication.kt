6539e0b90fef7521a2cf8c626fb21a20b02cddf8
Gelson Souza <gelson.majorit@gmail.com>
2023-03-31 22:09:26 -0300

    first commit

---
+++ src/main/kotlin/dev/gelson/requestcreditsystem/RequestCreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package dev.gelson.requestcreditsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestCreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestCreditSystemApplication>(*args)
+}
---
+++ src/test/kotlin/dev/gelson/requestcreditsystem/RequestCreditSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.gelson.requestcreditsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestCreditSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
